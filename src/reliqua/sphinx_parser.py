cd66b755c7038b1ca31e240e9b675a33754b7101
Terrence <tmeiczin@users.noreply.github.com>
2024-04-16 17:39:44 -0400

    remove print (#10)

--- src/reliqua/sphinx_parser.py
+++ src/reliqua/sphinx_parser.py
@@ -64,7 +64,6 @@ class SphinxParser:
         # rename in to location
         options["location"] = options.pop("in")
         options["required"] = True if "required" in string and "optional" not in string else False
-        print(options)
 
         return options
 
